50cac10db0451603b8a1a1f3a7ce54620eb283fc
RickDeckcard <50795196+RickDeckcard@users.noreply.github.com>
2019-05-29 00:29:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@
 
 
 //Address of the meme voting smart contract on the testnet of the aeternity blockchain
-const contractAddress = ' ct_bgQypSMnkYK3sJJ6UNyLL19dqoQyGJEjGteZsUK1abS4C3uAn';
+const contractAddress = 'ct_Hx4WhDGTjdiMxH8oMZDd8KxBwZmQdfa5zGirSbuNBpVRmH5hn';
 //Create variable for client so it can be used in different functions
 var client = null;
 //Create a new global array for the memes
